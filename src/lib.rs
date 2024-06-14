1284ecbd0186c6c4697f39e47f8115b6bf2b0c7c
danielcatalan <40647262+danielcatalan@users.noreply.github.com>
2024-06-14 21:10:36 +0000

    add c_for

--- src/lib.rs
+++ src/lib.rs
@@ -43,6 +43,17 @@ impl<const poly: usize> Crc<poly> {
 }
 
 
+macro_rules! c_for {
+    ($a:stmt; $b:expr; $c:expr; $d:expr) => {
+        {
+            $a;
+            while $b{
+                $d
+                $c;
+            }
+        }  
+    };
+}
 
 #[cfg(test)]
 mod tests {
@@ -66,5 +77,18 @@ mod tests {
 
         let x = Crc8::LUT[255];
         assert_eq!(0xF3, x);
+    }
+
+    #[test]
+    fn test_c_for() {
+        let mut x = 1;
+
+        // factorial of 5
+        // !5 => 5*4*3*2*1 => 120
+        c_for!(let mut i=1; i <= 5; i+=1; {
+            x *= i;
+        });
+
+        assert_eq!(120, x);
     }
 }

8963f3c843412b69e614efebac3d44e5430f2883
danielcatalan <40647262+danielcatalan@users.noreply.github.com>
2024-06-19 07:11:40 +0000

    implement crc16

--- src/crc.rs
+++ src/crc.rs
@@ -22,17 +22,40 @@ impl<const POLY: usize, const INIT: usize, REFL: BitReflecttion> Crc<u8, POLY, I
     }
 }
 
+impl<const POLY: usize, const INIT: usize, REFL: BitReflecttion> Crc<u16, POLY, INIT, REFL> {
+    const LUT: [u16; 256] = LutGenerator::<u16>::generate_lut(POLY as u16);
+
+    pub fn calc_crc(input: &[u8]) -> u16 {
+
+        let crc = input.iter()
+        .map(|b| REFL::process(*b))
+        .fold(INIT as u16, |crc, b| {
+            let data = (((crc>>8) as u8) ^ b) as usize;
+            (crc << 8) ^ (Self::LUT[data])
+        });
+        REFL::process(crc)
+    }
+}
 #[cfg(test)]
 mod tests {
     use super::*;
     type Crc8 = Crc<u8, 0x07, 0x00, NoReflect>;
+    type Crc16XModem = Crc::<u16,0x1021,0x0000,NoReflect>;
 
     #[test]
-    fn it_works() {
+    fn test_simple_crc8() {
         let input = "123456789";
 
         let crc = Crc8::calc_crc(input.as_bytes());
         assert_eq!(0xF4, crc);
+    }
+
+    #[test]
+    fn test_simple_crc16() {
+        let input = "123456789";
+        // using CRC16/XMODEM
+        let crc = Crc16XModem::calc_crc(input.as_bytes());
+        assert_eq!(0x31C3, crc);
     }
 
 

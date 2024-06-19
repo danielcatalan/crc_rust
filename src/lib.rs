5215d0080fdb4ff857722977d8eedb02fb897edb
danielcatalan <40647262+danielcatalan@users.noreply.github.com>
2024-06-19 07:44:56 +0000

    add more crc identities

--- src/lib.rs
+++ src/lib.rs
@@ -10,3 +10,6 @@ pub type Crc8Basic = Crc8<0x07, 0x00, NoReflect>;
 pub type Crc8Cdma2000 = Crc8<0x9B, 0xFF, NoReflect>;
 pub type Crc8Darc = Crc8<0x39, 0x00, Reflect>;
 pub type Crc8DvbS2 = Crc8<0xD5, 0x00, NoReflect>;
+
+pub type Crc16Ccitt = Crc16<0x1021, 0x0000, NoReflect>;
+pub type Crc16AugCcitt = Crc16<0x1021, 0x1D0F, NoReflect>;

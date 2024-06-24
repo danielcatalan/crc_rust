b158f66f4573ee1bbbbea98734e5f167868d64f4
danielcatalan <40647262+danielcatalan@users.noreply.github.com>
2024-06-24 18:04:34 +0000

    add implementation for crc32

--- src/crc.rs
+++ src/crc.rs
@@ -10,6 +10,10 @@ pub struct Crc16<const POLY: u16, const INIT: u16, REFL: BitReflecttion> {
     phantom: PhantomData<REFL>,
 }
 
+pub struct Crc32<const POLY: u32, const INIT: u32, REFL: BitReflecttion> {
+    phantom: PhantomData<REFL>,
+}
+
 impl<const POLY: u8, const INIT: u8, REFL: BitReflecttion> Crc8<POLY, INIT, REFL> {
     const LUT: [u8; 256] = LutGenerator::<u8>::generate_lut(POLY);
 
@@ -18,8 +22,8 @@ impl<const POLY: u8, const INIT: u8, REFL: BitReflecttion> Crc8<POLY, INIT, REFL
             .iter()
             .map(|b| REFL::process(*b))
             .fold(INIT, |crc, b| {
-                let data = (b ^ crc) as usize;
-                Self::LUT[data]
+                let pos = (b ^ crc) as usize;
+                Self::LUT[pos]
             });
         REFL::process(crc)
     }
@@ -33,8 +37,23 @@ impl<const POLY: u16, const INIT: u16, REFL: BitReflecttion> Crc16<POLY, INIT, R
             .iter()
             .map(|b| REFL::process(*b))
             .fold(INIT, |crc, b| {
-                let data = (((crc >> 8) as u8) ^ b) as usize;
-                (crc << 8) ^ (Self::LUT[data])
+                let pos = (((crc >> 8) as u8) ^ b) as usize;
+                (crc << 8) ^ (Self::LUT[pos])
+            });
+        REFL::process(crc)
+    }
+}
+
+impl<const POLY: u32, const INIT: u32, REFL: BitReflecttion> Crc32<POLY, INIT, REFL> {
+    const LUT: [u32; 256] = LutGenerator::<u32>::generate_lut(POLY);
+
+    pub fn calc_crc(input: &[u8]) -> u32 {
+        let crc = input
+            .iter()
+            .map(|b| REFL::process(*b))
+            .fold(INIT, |crc, b| {
+                let pos = ((crc ^ ((b as u32) <<24)) >> 24) as usize;
+                (crc << 8) ^ (Self::LUT[pos])
             });
         REFL::process(crc)
     }
@@ -45,6 +64,7 @@ mod tests {
     use super::*;
     type Crc8Basic = Crc8<0x07, 0x00, NoReflect>;
     type Crc16XModem = Crc16<0x1021, 0x0000, NoReflect>;
+    type Crc32Jamcrc  = Crc32<0x04C11DB7, 0xFFFFFFFF, Reflect>;
 
     #[test]
     fn test_simple_crc8() {
@@ -60,5 +80,13 @@ mod tests {
         // using CRC16/XMODEM
         let crc = Crc16XModem::calc_crc(input.as_bytes());
         assert_eq!(0x31C3, crc);
+    }
+
+    #[test]
+    fn test_simple_crc32() {
+        let input = "123456789";
+        // using CRC-32/JAMCRC
+        let crc = Crc32Jamcrc::calc_crc(input.as_bytes());
+        assert_eq!(0x340BC6D9, crc);
     }
 }
--- src/lut_generator.rs
+++ src/lut_generator.rs
@@ -57,6 +57,32 @@ impl LutGenerator<u16> {
     }
 }
 
+impl LutGenerator<u32> {
+    #[inline]
+    pub const fn generate_lut(poly: u32) -> [u32; 256] {
+        let generator = poly;
+        let mut table = [0; 256];
+
+        c_for!(let mut dividend = 0; dividend < 256; dividend += 1; {
+            let mut currbyte = (dividend << 24) as u32;
+
+            c_for!(let mut bit = 0; bit < 8; bit +=1; {
+                if currbyte & 0x80000000 != 0 {
+                    currbyte <<=1;
+                    currbyte ^= generator;
+                }
+                else {
+                    currbyte <<=1;
+                }
+            });
+
+            table[dividend] = currbyte;
+        });
+
+        table
+    }
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
@@ -85,5 +111,18 @@ mod tests {
 
         let x = TABLE[255];
         assert_eq!(0x1EF0, x);
+    }
+
+    #[test]
+    fn test_crc32_table() {
+        const TABLE: [u32; 256] = LutGenerator::<u32>::generate_lut(0x04C11DB7);
+        let x = TABLE[0];
+        assert_eq!(0x0000, x);
+
+        let x = TABLE[1];
+        assert_eq!(0x04C11DB7, x);
+
+        let x = TABLE[255];
+        assert_eq!(0xB1F740B4, x);
     }
 }
